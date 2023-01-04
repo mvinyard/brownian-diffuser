16ce47b5b9d997db391756393779d1997d33575d
mvinyard <mvinyard.ai@gmail.com>
2023-01-04 06:55:34 +0000

    add wf

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import re
+import os
+import sys
+
+setup(
+    name="brownian-diffuser",
+    version="0.0.0",
+    python_requires=">3.7.0",
+    author="Michael E. Vinyard - Harvard University - Broad Institute of MIT and Harvard - Massachussetts General Hospital",
+    author_email="mvinyard@broadinstitute.org",
+    url="https://github.com/mvinyard/sc-neural-diffeqs",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    description="Brownian diffuser",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "torch>=1.12.0",
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    license="MIT",
+)
