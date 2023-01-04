20667bf0c2d0e7e152bfe446604d54bada07173e
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2023-01-04 15:43:10 -0700

    Update _nn_int.py

--- brownian_diffuser/_core/_nn_int.py
+++ brownian_diffuser/_core/_nn_int.py
@@ -1,8 +1,17 @@
 
-from ._time import timespan
+__module_name__ = "_nn_int.py"
+__doc__ = """Brownian Diffusion module formatted for standalone integration."""
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu"])
+__version__ = "0.0.2"
+
+
+# -- import local dependencies: ----------------------------------------------------------
 from ._brownian_diffuser import BrownianDiffuser
+from ._time import timespan
 
 
+# -- code: -------------------------------------------------------------------------------
 def nn_int(net, X0, t, dt=0.1, stdev=0.5, max_steps=None, return_all=False):
 
     n_steps = int(timespan(t) / dt)
